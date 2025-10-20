44c51a5dc2a74ec72a9bbc46ec4b8500566f6d9b
DatTran <trandat5079@gmail.com>
2025-10-20 17:37:15 +0700

    add SignUpScreen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default function App() {
   return <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
       
           <AppNavigator />
+          <SignupScreen/>
         
       </SafeAreaView>
     </SafeAreaProvider>;
