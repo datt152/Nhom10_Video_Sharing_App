799351064cdf362075d7dfa84c756bd8c65ab0ec
DatTran <trandat5079@gmail.com>
2025-10-20 17:39:15 +0700

    add AuthNavigator and change component in App.tsx

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import SignupScreen from './src/screens/SignupScreen';
+import AuthNavigator from './src/navigation/AuthNavigator';
 
 export default function App() {
   return <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
       
-          <AppNavigator />
-          <SignupScreen/>
+          <AuthNavigator />
         
       </SafeAreaView>
     </SafeAreaProvider>;
