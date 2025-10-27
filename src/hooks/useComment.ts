f4bf246cdb71205926f7630796b5d1680bbafe12
DatTran <trandat5079@gmail.com>
2025-10-27 15:41:32 +0700

    change host from api to localhost

--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.1.52:3000';
+const API_BASE_URL = 'http://localhost:3000';
 const CURRENT_USER_ID = 'u1';
 
 interface Comment {
